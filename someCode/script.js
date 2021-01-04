213d379267ae2c36cf4b59372bd53cdf76c9270c
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-01-04 17:36:48 +0300

    add two script element

---
+++ someCode/script.js
@@ -0,0 +1,9 @@
+const massMark = 78;
+const heighMark = 1.69;
+const massJohn = 92;
+const heighJohn = 1.95;
+
+const BMIMark = massMark / heighMark ** 2;
+const BMIJohn = massJohn / (heighJohn * heighJohn);
+
+console.log(BMIJohn, BMIMark);
