35891ab59bab70eecdd580035f9e3b76344ee31d
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-08 16:07:09 +0300

    test is somthing else

--- Game With JS/script.js
+++ Game With JS/script.js
@@ -1,2 +1,5 @@
 'use strict';
 console.log(document.querySelector('.message'));
+var message = document.querySelector('.message');
+var score = document.querySelector('.score');
+var highScore = document.querySelector('.highscore');
