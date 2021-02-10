35e3740c71dc4d06f24a53a62cd8893627f32ee3
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-10 14:51:25 +0300

    Game element have provider

--- Game With JS/script.js
+++ Game With JS/script.js
@@ -1,9 +1,45 @@
 'use strict';
-console.log(document.querySelector('.message'));
+
 var message = document.querySelector('.message');
 var score = document.querySelector('.score');
 var highScore = document.querySelector('.highscore');
 var checkButton = document.querySelector('.check');
+var numberCenter = document.querySelector('.guess');
+var number = document.querySelector('.number');
+let thescore = 20;
 checkButton.addEventListener('click', function () {
-  console.log('test');
+  var currentNumber = Number(numberCenter.value);
+  if (currentNumber == 0) {
+    message.textContent = 'No number';
+    return;
+  }
+  if (currentNumber == generateNumber) {
+    message.textContent = '😂 Correct number';
+    number.textContent = generateNumber;
+  } else if (currentNumber < generateNumber) {
+    message.textContent = 'Too low';
+    thescore--;
+    score.textContent = thescore;
+  } else if (currentNumber > generateNumber) {
+    message.textContent = 'Too high';
+    thescore--;
+    score.textContent = thescore;
+  }
 });
+
+var generateNumber = Math.floor(Math.random() * 20 + 1);
+
+var checkFuntion = function () {
+  var currentNumber = Number(numberCenter.Value);
+  if (currentNumber == null) {
+    message.textContent = 'No number';
+  }
+  if (currentNumber == generateNumber) {
+    message.textContent = '😂 Correct number';
+    number.textContent = generateNumber;
+  } else if (currentNumber < generateNumber) {
+    message.textContent = 'Too low';
+  } else if (currentNumber > generateNumber) {
+    message.textContent = 'Too high';
+  }
+};
