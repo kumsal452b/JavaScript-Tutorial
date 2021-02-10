36a7bce6d693d0964146169b0bdca1356815f5a8
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-10 13:22:04 +0300

    addEventlıstener ın js code

--- Game With JS/script.js
+++ Game With JS/script.js
@@ -3,3 +3,7 @@ console.log(document.querySelector('.message'));
 var message = document.querySelector('.message');
 var score = document.querySelector('.score');
 var highScore = document.querySelector('.highscore');
+var checkButton = document.querySelector('.check');
+checkButton.addEventListener('click', function () {
+  console.log('test');
+});
