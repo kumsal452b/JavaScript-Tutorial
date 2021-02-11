2f8f999bc4cbafc7f453cfb30cd6f14e254aed4b
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-11 18:33:13 +0300

    change some trouble

--- Learn-Model/script.js
+++ Learn-Model/script.js
@@ -2,3 +2,4 @@
 const model = document.querySelector('.model');
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.close-model');
+const btnOpenModal = document.querySelector('.show-modal');
