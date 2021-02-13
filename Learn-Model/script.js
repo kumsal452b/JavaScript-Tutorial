76ba6a1ea00c05173bbf6d7d539a7a99a3515523
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-13 18:58:37 +0300

    new code have added

--- Learn-Model/script.js
+++ Learn-Model/script.js
@@ -3,16 +3,21 @@ const model = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-var name1 = function () {};
 
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', function () {
-    console.log('merhaba');
-    model.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-btnClose.addEventListener('click', function () {
+const closeModal = function () {
   model.classList.add('hidden');
-  //   overlay.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const openModal = function () {
+  console.log('merhaba');
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
+}
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+model.addEventListener('keyup', e => {
+  console.log('selam');
 });
