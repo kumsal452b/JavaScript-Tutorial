484a58e26d37dc2ec41089f2d6bdaf7a78c66bda
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-13 12:43:26 +0300

    add new  code callaed element

--- Learn-Model/script.js
+++ Learn-Model/script.js
@@ -1,7 +1,18 @@
 'use strict';
-const model = document.querySelector('.model');
+const model = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnClose = document.querySelector('.close-model');
+const btnClose = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
+var name1 = function () {};
 
-for (let i = 0; i < btnOpenModal.length; i++) {}
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', function () {
+    console.log('merhaba');
+    model.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+btnClose.addEventListener('click', function () {
+  model.classList.add('hidden');
+  //   overlay.classList.add('hidden');
+});
