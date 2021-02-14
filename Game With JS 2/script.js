afb5ea76674704e46b08ba2491887f2899b4751f
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-14 20:23:54 +0300

    add three button element

--- Game With JS 2/script.js
+++ Game With JS 2/script.js
@@ -2,6 +2,8 @@
 const score1 = document.querySelector('#score--0');
 const score2 = document.querySelector('#score--1');
 const dice = document.querySelector('.dice');
-
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 score1.textContent = 0;
 score2.textContent = 0;
