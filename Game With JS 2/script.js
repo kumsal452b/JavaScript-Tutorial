31b9a902698fb1a04831d73e47c6551100807357
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-14 19:45:48 +0300

    add new dice element from js file

--- Game With JS 2/script.js
+++ Game With JS 2/script.js
@@ -1,6 +1,7 @@
 'use strict';
-const score1 = document.querySelector('#score--1');
-const score2 = document.querySelector('#score--2');
+const score1 = document.querySelector('#score--0');
+const score2 = document.querySelector('#score--1');
+const dice = document.querySelector('.dice');
 
-score1.textContent = '0';
+score1.textContent = 0;
 score2.textContent = 0;
