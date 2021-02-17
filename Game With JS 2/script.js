ea4f2115465512a489dd1b76d08c307e86ca0c9f
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-17 16:06:07 +0300

    Add new event listener

--- Game With JS 2/script.js
+++ Game With JS 2/script.js
@@ -47,7 +47,7 @@ btnHold.addEventListener('click', function () {
     scorList[activePlayer] += currentScor;
     document.querySelector(`#score--${activePlayer}`).textContent =
       scorList[activePlayer];
-    if (scorList[activePlayer] >= 25) {
+    if (scorList[activePlayer] >= 20) {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -58,5 +58,26 @@ btnHold.addEventListener('click', function () {
     } else {
       swichPlayer();
     }
+  }
+});
+
+btnNew.addEventListener('click', function () {
+  if (!playing) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--winner');
+    playing = true;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+    activePlayer = 0;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--active');
+
+    scorList = [0, 0];
+    currentScor = 0;
+    document.querySelector(`#score--${0}`).textContent = 0;
+    document.querySelector(`#score--${1}`).textContent = 0;
   }
 });
