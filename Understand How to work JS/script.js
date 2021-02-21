d17be16748be93077d801d869c3c66f6a733392f
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-21 11:36:10 +0300

    some element have tested

--- Understand How to work JS/script.js
+++ Understand How to work JS/script.js
@@ -1,4 +1,15 @@
 'use strict';
 
-console.log(a);
-const a = 'selam';
+const yahya = {
+  firstName: 'Yahya',
+  year: '1996',
+  calcAge: () => {
+    console.log(`Hey ${this.firstName}`);
+  },
+  test: function () {
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+
+yahya.calcAge();
+yahya.test();
